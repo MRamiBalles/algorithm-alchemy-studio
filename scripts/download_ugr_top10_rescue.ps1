2c752e2063b1975dd557de1894f0f043a939b92d
Manuel Ramirez <ramiballes96@gmail.com>
2026-02-15 14:29:15 +0100

    feat: Add UGR official documents and a PowerShell script for downloading them.

---
+++ scripts/download_ugr_top10_rescue.ps1
@@ -0,0 +1,138 @@
+$baseDir = "D:\AA-1\algorithm-alchemy-studio\public\content\official_docs\ugr"
+$degrees = @(
+    @{ Name = "Administracin y Direccin de Empresas"; Links = @(
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351115.pdf", # Economia Politica
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351112.pdf", # Fundamentos Direccion
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351114.pdf", # Intro Operaciones Financieras
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351113.pdf", # Intro Marketing
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351111.pdf", # Matematicas
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351119.pdf", # Contabilidad General
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351118.pdf", # Historia Desarrollo Economico
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351116.pdf", # Intro Derecho
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/235111A.pdf", # Matematicas Empresariales
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351117.pdf", # Microeconomia
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351124.pdf", # Contabilidad Financiera I
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351125.pdf", # Direccion Comercial
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351123.pdf", # Direccion y Administracion
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351121.pdf", # Macroeconomia
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351122.pdf", # Tecnicas Cuantitativas I
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351126.pdf", # Analisis Operaciones Financieras
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351127.pdf", # Contabilidad Financiera II
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/235112A.pdf", # Economia Mundial
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351129.pdf", # Organizacion de Empresas
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351128.pdf", # Tecnicas Cuantitativas II
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351131.pdf", # Contabilidad Gestion
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351132.pdf", # Derecho Fiscal
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351134.pdf", # Direccion Operaciones I
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351135.pdf", # Direccion Estrategica I
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351133.pdf", # Econometria
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351136.pdf", # Analisis Estados Financieros
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351138.pdf", # RRHH I
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351139.pdf", # Direccion Estrategica II
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351137.pdf", # Economia Española
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/235113A.pdf", # Metodos Cuantitativos
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/23511B2.pdf", # Auditoria
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/23511A1.pdf", # Comercio Exterior
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351142.pdf", # Creacion Empresas
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/23511E2.pdf", # Direccion Operaciones II
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2351141.pdf", # Direccion Financiera
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/23511M2.pdf", # Distribucion Comercial
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/23511D1.pdf", # Gestion Empresas Internacional
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/23511E1.pdf", # Calidad Empresarial
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/23511C1.pdf", # Medioambiental
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/23511A2.pdf" # Investigacion Mercados
+        ) 
+    };
+    @{ Name = "Antrologa Social y Cultural"; Links = @(
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2041112.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/204111A.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2041113.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2041125.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/20411AB.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2041132.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2041144.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/20411P3.pdf"
+        ) 
+    };
+    @{ Name = "Arqueologa"; Links = @(
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/1151113.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/1151114.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/1151115.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/1151111.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/1151112.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/1151121.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/1151131.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/1151141.pdf"
+        ) 
+    };
+    @{ Name = "Bellas Artes"; Links = @(
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2051111.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2051113.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2051125.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2051137.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/20511A5.pdf"
+        ) 
+    };
+    @{ Name = "Biologa"; Links = @(
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2011112.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2011125.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2011136.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2011145.pdf"
+        ) 
+    };
+    @{ Name = "Bioqumica"; Links = @(
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2641115.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2641126.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2641135.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2641144.pdf"
+        ) 
+    };
+    @{ Name = "Biotecnologa"; Links = @(
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2511111.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2511123.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2511132.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2511142.pdf"
+        ) 
+    };
+    @{ Name = "Ciencia y Tecnologa de los Alimentos"; Links = @(
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2031115.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2031121.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2031133.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2031145.pdf"
+        ) 
+    };
+    @{ Name = "Ciencias Ambientales"; Links = @(
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2061111.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2061121.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2061133.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2061141.pdf"
+        ) 
+    };
+    @{ Name = "Ciencias de la Actividad Fsica y del Deporte"; Links = @(
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2881111.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2881121.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/2881133.pdf",
+            "https://grados.ugr.es/sites/grados/default/public/guias-firmadas/2024-2025/28811M9.pdf"
+        ) 
+    }
+)
+
+foreach ($degree in $degrees) {
+    # Match existing folder using wildcard for encoding safety
+    $targetFolder = Get-ChildItem $baseDir -Directory | Where-Object { $_.Name -like "*$($degree.Name)*" } | Select-Object -First 1
+    
+    if ($targetFolder) {
+        Write-Host "Populating $($targetFolder.Name)..."
+        foreach ($link in $degree.Links) {
+            $fileName = $link.Split('/')[-1]
+            $destPath = Join-Path $targetFolder.FullName $fileName
+            if (-not (Test-Path $destPath)) {
+                Write-Host "Downloading $fileName to $($targetFolder.Name)"
+                Invoke-WebRequest -Uri $link -OutFile $destPath -ErrorAction SilentlyContinue
+            }
+        }
+    }
+    else {
+        Write-Warning "Folder for $($degree.Name) not found in filesystem. Skipping."
+    }
+}
